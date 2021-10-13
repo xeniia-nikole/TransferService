0e84bce021e6a2eccdf429c9fc81612de092d427
Kseniia <xenia-nikole@mail.ru>
2021-10-13 14:53:09 +0300

    redo create_fileOperationsLogs task

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM adoptopenjdk/openjdk11:alpine-jre
 
 EXPOSE 5500
 
-ADD target/transfer_service-0.0.1-SNAPSHOT.jar transfer_service.jar
+ADD build/libs/transfer_service-0.0.1-SNAPSHOT.jar transfer_service.jar
 
 ENTRYPOINT ["java", "-jar", "/transfer_service.jar"]
--- src/main/java/com/transfer/MoneyTransferServiceApplication.java
+++ src/main/java/com/transfer/MoneyTransferServiceApplication.java
@@ -6,6 +6,9 @@ import com.transfer.repository.MoneyTransferRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,28 +17,52 @@ import java.util.Map;
 @SpringBootApplication
 public class MoneyTransferServiceApplication {
 
+    public static final String nameLog = "fileOperatiosLogs.log";
     public static final String time = new SimpleDateFormat("HH:mm:ss dd.MM.yyyy").format(new Date());
 
     public static void main(String[] args) {
         SpringApplication.run(MoneyTransferServiceApplication.class, args);
         System.out.println("\nWelcome to MoneyTransferService!");
+        createFiles();
         addCards();
+
         printRepo();
     }
 
+    public static void createFiles() {
+
+        String msgLog = "Файл fileOperatiosLogs.log успешно создан";
+        File logFile = new File(nameLog);
+
+        if (!logFile.exists()) {
+            try {
+                if (logFile.createNewFile())
+                    System.out.println(msgLog);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try (FileWriter writerLogs = new FileWriter(nameLog, true)) {
+                writerLogs.write("Время операции:" + time + ": " + msgLog + "\n");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void addCards() {
-        MoneyTransferRepository.cardsRepository.put("1111111111111111",
-                new Card("1111111111111111",
+        MoneyTransferRepository.cardsRepository.put("01000100001000010",
+                new Card("01000100001000010",
                         "11/21",
                         "111",
                         new AmountCard(BigDecimal.valueOf(203_345.15), "RUR")));
-        MoneyTransferRepository.cardsRepository.put("1111111111111112",
-                new Card("1111111111111112",
+        MoneyTransferRepository.cardsRepository.put("01000100001000011",
+                new Card("01000100001000011",
                         "11/22",
                         "112",
                         new AmountCard(BigDecimal.valueOf(345.15), "RUR")));
-        MoneyTransferRepository.cardsRepository.put("1111111111111113",
-                new Card("1111111111111113",
+        MoneyTransferRepository.cardsRepository.put("01000100001000001",
+                new Card("01000100001000001",
                         "11/23",
                         "113",
                         new AmountCard(BigDecimal.valueOf(203_345_111.15), "RUR")));
@@ -49,23 +76,4 @@ public class MoneyTransferServiceApplication {
         }
 
     }
-//
-//        public static void createFiles() {
-//        String msgLog = "Файл fileOperatiosLogs.log успешно создан";
-//        File logFile = new File(nameLog);
-//        if (!logFile.exists()) {
-//            try {
-//                if (logFile.createNewFile())
-//                    System.out.println(msgLog);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            try (FileWriter writerLogs = new FileWriter(nameLog, true)) {
-//                writerLogs.write("Время операции:" + time + ": " + msgLog + "\n");
-//
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
 }
